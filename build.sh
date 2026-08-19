d5588636812ca106ddfd62ea916b8e6964c2ca65
ximiyad123 <ximiyadka124@proton.me>
2026-08-19 18:47:46 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -xe
+
+[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
+./build/build.sh "$@"
+./make-bootimages.sh "$@"
